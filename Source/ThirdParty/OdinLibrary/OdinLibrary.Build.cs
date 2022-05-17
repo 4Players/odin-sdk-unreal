951279003c8ee09b4134cb4f77b116a586ce9b24
Sven Paulsen <info@sven-paulsen.de>
2022-05-17 13:51:43 +0200

    work on platform support

--- Source/ThirdParty/OdinLibrary/OdinLibrary.Build.cs
+++ Source/ThirdParty/OdinLibrary/OdinLibrary.Build.cs
@@ -4,28 +4,42 @@ using System.Diagnostics;
 
 public class OdinLibrary : ModuleRules
 {
-  public OdinLibrary(ReadOnlyTargetRules Target) : base(Target)
-  {
-    Type = ModuleType.External;
+    public OdinLibrary(ReadOnlyTargetRules Target) : base(Target)
+    {
+        Type = ModuleType.External;
 
-    if (Target.Platform == UnrealTargetPlatform.Win64)
-    {
-      // Add the import library
-      PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "x64", "Win", "odin.lib"));
-      PublicDelayLoadDLLs.Add("odin.dll");
-      RuntimeDependencies.Add("$(TargetOutputDir)/odin.dll", "$(PluginDir)/Source/ThirdParty/OdinLibrary/x64/Win/odin.dll");
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            string modulePath = Path.Combine(ModuleDirectory, "x64", "Win");
+            PublicAdditionalLibraries.Add(Path.Combine(modulePath, "odin.lib")); ;
+            PublicDelayLoadDLLs.Add("odin.dll");
+            RuntimeDependencies.Add("$(TargetOutputDir)/odin.dll", "$(PluginDir)/Source/ThirdParty/OdinLibrary/x64/Win/odin.dll");
+        }
+        else if (Target.Platform == UnrealTargetPlatform.Mac)
+        {
+            string odinDylibPath = Path.Combine(ModuleDirectory, "x64", "Mac", "libodin.dylib");
+            PublicAdditionalLibraries.Add(odinDylibPath);
+            PublicDelayLoadDLLs.Add(odinDylibPath);
+            RuntimeDependencies.Add("$(TargetOutputDir)/libodin.dylib", odinDylibPath);
+        }
+        // else if (Target.Platform == UnrealTargetPlatform.LinuxArm64)
+        // {
+        //     string odinSoPath = Path.Combine(ModuleDirectory, "arm64", "Linux", "libodin.so");
+        //     PublicAdditionalLibraries.Add(odinSoPath);
+        //     PublicDelayLoadDLLs.Add(odinSoPath);
+        //     RuntimeDependencies.Add("$(TargetOutputDir)/libodin.so", odinSoPath);
+        // }
+        else if (Target.Platform == UnrealTargetPlatform.Linux)
+        {
+            string odinSoPath = Path.Combine(ModuleDirectory, "x64", "Linux", "libodin.so");
+            PublicAdditionalLibraries.Add(odinSoPath);
+            PublicDelayLoadDLLs.Add(odinSoPath);
+            RuntimeDependencies.Add("$(TargetOutputDir)/libodin.so", odinSoPath);
+        }
+        else if (Target.Platform == UnrealTargetPlatform.Android)
+        {
+            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "arm64", "Android", "libodin.so"));
+            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "x64", "Android", "libodin.so"));
+        }
     }
-    else if (Target.Platform == UnrealTargetPlatform.Mac)
-    {
-      string odinDylibPath = Path.Combine("$(PluginDir)", "Source", "ThirdParty", "OdinLibrary", "x64", "Mac", "libodin.dylib");
-      PublicAdditionalLibraries.Add(odinDylibPath);
-      RuntimeDependencies.Add("$(TargetOutputDir)/libodin.dylib", odinDylibPath);
-    }
-    else if (Target.Platform == UnrealTargetPlatform.Linux)
-    {
-      string odinSoPath = Path.Combine("$(PluginDir)", "Source", "ThirdParty", "OdinLibrary", "x64", "Linux", "libodin.so");
-      PublicAdditionalLibraries.Add(odinSoPath);
-      RuntimeDependencies.Add("$(TargetOutputDir)/libodin.so", odinSoPath);
-    }
-  }
 }
