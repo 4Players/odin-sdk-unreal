078227061bfd71a9e0d01b867947be1d35eec94e
Sven Paulsen <info@sven-paulsen.de>
2022-05-17 13:50:58 +0200

    return 'NumSamples' instead of 0 when no stream has been assigned to the sound generator

--- Source/Odin/Private/OdinMediaSoundGenerator.cpp
+++ Source/Odin/Private/OdinMediaSoundGenerator.cpp
@@ -7,12 +7,12 @@ OdinMediaSoundGenerator::OdinMediaSoundGenerator() = default;
 int32 OdinMediaSoundGenerator::OnGenerateAudio(float *OutAudio, int32 NumSamples)
 {
     if (stream_handle_ == 0) {
-        return 0;
+        return NumSamples;
     }
 
     auto read = odin_audio_read_data(stream_handle_, OutAudio, NumSamples, OdinChannelLayout_Mono);
     if (odin_is_error(read)) {
-        return 0;
+        return NumSamples;
     } else {
         return read;
     }
