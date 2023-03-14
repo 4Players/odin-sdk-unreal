47bb784b04fce3e5ef3a92eb9cd7b5a938065d6d
Sven Paulsen <info@sven-paulsen.de>
2023-03-14 09:23:47 +0100

    use stereo for playback

--- Source/Odin/Private/Odin.cpp
+++ Source/Odin/Private/Odin.cpp
@@ -63,7 +63,7 @@ void FOdinModule::StartupModule()
         UE_LOG(Odin, Log, TEXT("Loaded Library (%s)"), *(LibraryPath / libraryName));
     }
 #endif
-    odin_startup_ex(ODIN_VERSION, 48000, OdinChannelLayout_Mono);
+    odin_startup_ex(ODIN_VERSION, 48000, OdinChannelLayout_Stereo);
 }
 
 void FOdinModule::ShutdownModule()
