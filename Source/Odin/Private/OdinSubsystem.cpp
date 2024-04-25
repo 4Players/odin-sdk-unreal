37c14bcf655daff55eb042ba847382f8b625b545
Sven Paulsen <info@sven-paulsen.de>
2024-04-25 17:09:27 +0200

    add subsystem to store rooms

---
+++ Source/Odin/Private/OdinSubsystem.cpp
@@ -0,0 +1,59 @@
+﻿/* Copyright (c) 2022-2024 4Players GmbH. All rights reserved. */
+
+#include "OdinSubsystem.h"
+
+#include "Odin.h"
+#include "OdinRoom.h"
+
+UOdinSubsystem* UOdinSubsystem::Get()
+{
+    if (GEngine) {
+        return GEngine->GetEngineSubsystem<UOdinSubsystem>();
+    }
+    return nullptr;
+}
+
+void UOdinSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+    UE_LOG(Odin, Log, TEXT("Initialize Odin Subsystem"));
+}
+
+void UOdinSubsystem::Deinitialize()
+{
+    Super::Deinitialize();
+    UE_LOG(Odin, Log, TEXT("Deinitialize Odin Subsystem"));
+}
+
+void UOdinSubsystem::RegisterRoom(OdinRoomHandle RoomHandle, UOdinRoom* Room)
+{
+    FScopeLock RegisterRoomLock(&RegisteredRoomsLock);
+    UE_LOG(Odin, Log, TEXT("Registering Odin Room with room handle %llu"), RoomHandle);
+    RegisteredRooms.Add(RoomHandle, TWeakObjectPtr<UOdinRoom>(Room));
+}
+
+void UOdinSubsystem::DeregisterRoom(OdinRoomHandle RoomHandle)
+{
+    FScopeLock DeregisterRoomLock(&RegisteredRoomsLock);
+    if (RegisteredRooms.Contains(RoomHandle)) {
+        UE_LOG(Odin, Log, TEXT("Deregistering Odin Room with room handle %llu"), RoomHandle);
+        RegisteredRooms.Remove(RoomHandle);
+    } else {
+        UE_LOG(Odin, Warning,
+               TEXT("Failed deregistering Odin room with room handle %llu - room handle was never "
+                    "registered."),
+               RoomHandle);
+    }
+}
+
+TWeakObjectPtr<UOdinRoom> UOdinSubsystem::GetRoomByHandle(OdinRoomHandle RoomHandle)
+{
+    FScopeLock                       GetRoomLock(&RegisteredRoomsLock);
+    const TWeakObjectPtr<UOdinRoom>* OdinRoom = RegisteredRooms.Find(RoomHandle);
+    if (OdinRoom) {
+        UE_LOG(Odin, Verbose, TEXT("Retrieved Odin Room with room handle %llu"), RoomHandle);
+        return *OdinRoom;
+    }
+    UE_LOG(Odin, Verbose, TEXT("Did not find Odin Room with room handle %llu"), RoomHandle);
+    return nullptr;
+}
---
+++ Source/Odin/Public/OdinSubsystem.h
@@ -0,0 +1,29 @@
+﻿/* Copyright (c) 2022-2024 4Players GmbH. All rights reserved. */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "odin_sdk.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "OdinSubsystem.generated.h"
+
+class UOdinRoom;
+
+UCLASS()
+class ODIN_API UOdinSubsystem : public UEngineSubsystem
+{
+    GENERATED_BODY()
+  public:
+    static UOdinSubsystem* Get();
+
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+    virtual void Deinitialize() override;
+
+    void                      RegisterRoom(OdinRoomHandle RoomHandle, UOdinRoom* Room);
+    void                      DeregisterRoom(OdinRoomHandle RoomHandle);
+    TWeakObjectPtr<UOdinRoom> GetRoomByHandle(OdinRoomHandle RoomHandle);
+
+  protected:
+    TMap<OdinRoomHandle, TWeakObjectPtr<UOdinRoom>> RegisteredRooms;
+    FCriticalSection                                RegisteredRoomsLock;
+};
